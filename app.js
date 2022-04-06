d78be20578eff0827d8b9eff91c134042c30632e
kobekobe0 <72067397+kobekobe0@users.noreply.github.com>
2022-04-06 21:42:48 +0800

    learned basic CRUD with MongoDB

--- app.js
+++ app.js
@@ -1,11 +1,38 @@
 const express = require('express')
 const app = express()
 const mongoose = require('mongoose')
+app.use(express.json())
 
-mongoose.connect('mongodb://localhost/test')
-
-app.get('/', (req, res) => {
-    res.send('Hello World!')
+const Todo = require('./models/todo')
+mongoose.connect('mongodb://localhost/firstmongo')
+app.get('/', async (req, res) => {
+    const records = await Todo.find({ field1: 'field2' })
+    res.json(records)
+})
+app.post('/', async (req, res) => {
+    const body = req.body
+    //Create to the database
+    await Todo.create(body) //moongose method
+    res.json(body)
+})
+app.put('/', async (req, res) => {
+    const { record, newRecord } = req.body
+    const response = await Todo.updateOne(
+        {
+            record: record,
+        },
+        {
+            $set: {
+                record: newRecord,
+            },
+        }
+    )
+    res.json(response)
+})
+app.delete('/:record', async (req, res) => {
+    const record = req.params.record
+    const response = await Todo.deleteOne({ record: record })
+    res.json(response)
 })
 
 app.listen(3000, () => {
